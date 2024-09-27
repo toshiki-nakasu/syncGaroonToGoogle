723df1d1814a9c35ca1180aa8c3ed6f84b0e56bb
toshiki-nakasu <toshiki-nakasu@mamezou.com>
2024-09-27 00:52:57 +0000

    ADD: comment

--- sync-garoon-to-google/src/properties/ScriptProperties.js
+++ sync-garoon-to-google/src/properties/ScriptProperties.js
@@ -3,8 +3,13 @@ function setScriptProperties() {
     GaroonDomain: 'mamezo-dhd.cybozu.com',
     GaroonUser: 'toshiki-nakasu',
     GaroonPassword: 'Tosi+0022',
+    WorkTimeStart: '8:00',
+    WorkTimeEnd: '21:00',
     SyncDaysBefore: '30',
     SyncDaysAfter: '90',
     CalendarName: 'Garoon',
   });
 }
+
+// TODO WorkTime外にSyncを実行しない
+// TODO workflowでclasp push
